bf6654dc3bcc6bccfc9690d61e46dc0e6ab84566
liufeiit <liufei_it@126.com>
2014-09-16 23:55:58 +0800

    dd

--- deploy.win.sh
+++ deploy.win.sh
@@ -3,3 +3,5 @@ mvn install
 echo "copy war..."
 cp Analytics-Web/target/Analytics-Web.war D://software/jboss-as-web-7.0.1.Final/standalone/deployments
 echo "deploy success!"
+
+sh D:/software/jboss-as-web-7.0.1.Final/bin/standalone.sh
