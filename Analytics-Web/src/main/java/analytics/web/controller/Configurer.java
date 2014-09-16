3436e355d0630a384204286055ce7045e495eb7e
liufeiit <liufei_it@126.com>
2014-09-16 15:17:16 +0800

    sss

---
+++ Analytics-Web/src/main/java/analytics/web/controller/Configurer.java
@@ -0,0 +1,21 @@
+package analytics.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 
+ * @author 刘飞 E-mail:liufei_it@126.com
+ * @version 1.0
+ * @since 2014年9月16日 下午3:15:51
+ */
+public class Configurer extends BaseController {
+
+	@RequestMapping(value = "/configurer.htm")
+	public ModelAndView configurer_page(HttpServletRequest request) {
+		ModelAndView mv = newViewWithUser(request, "configurer", "在线参数", "参数概况");
+		return mv;
+	}
+}
