2d36b3eed7288d1643caa617269627e698342c95
liufeiit <liufei_it@126.com>
2014-09-19 18:07:58 +0800

    ss

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ mvn install
 echo "copy war..."
 cp Analytics-Web/target/Analytics-Web.war ~/dev/jboss-as-web-7.0.2.Final/standalone/deployments/
 echo "deploy success!"
+
+scp Analytics/Analytics/Analytics-Web/target/Analytics-Web.war ubuntu@stats:/home/ubuntu/soft/jboss-as-web-7.0.2.Final/standalone/deployments/
+echo "scp to stats server success."
 sh /home/lf/dev/jboss-as-web-7.0.2.Final/bin/standalone.sh
 
